90c64113045673ce87d5c8320e48c159a7def5ac
khanhduy2311 <87161173+khanhduy2311@users.noreply.github.com>
2022-12-18 20:56:23 +0700

    Create DOANP2.cpp

---
+++ DOANP2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, s, d;
+    map<int, int> mp;
+    map<int, int>::iterator it;
+    cin >> n;
+    while(n--){
+        cin >> s >> d;
+        it = mp.find(s);
+        if(it != mp.end()){
+            it->second = max(it->second, d);
+        }
+        else{
+            mp[s] = d;
+        }
+    }
+    it = mp.begin();
+    long long sum = it->second - it->first;
+    int Max = it->second;
+    for(++it; it != mp.end(); ++it){
+        if(it->first >= Max){
+            sum += it->second - it->first;
+        }
+        else if(it->second > Max){
+            sum += it->second - Max;
+        }
+        Max = max(Max, it->second);
+    }
+    cout << sum;
+    return 0;
+}
+    
